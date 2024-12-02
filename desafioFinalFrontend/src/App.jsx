d46da04585562a2d29797090b71d4388ea50bc75
beazinat <beatrizgarodrigues@gmail.com>
2024-12-02 09:35:53 -0300

    :tada: First Commit

---
+++ desafioFinalFrontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header.jsx'
+
+export default function App() {
+  return (
+    <section>
+      <Header />
+    </section>
+  )
+  
+}
---
+++ desafioFinalFrontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Desafio Final</h1>
+        </header>
+    )
+}
